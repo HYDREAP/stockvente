04dddca4463b29639b94dee4b6054a54a22c16fb
bianconejo <bianconejo4dev@gmail.com>
2013-06-04 10:51:32 +0200

    fusion youssef/arnaud

---
+++ StockVente/src/java/servlets/ConnexionServlet.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+
+public class ConnexionServlet extends HttpServlet {
+
+    private static final String VUE             = "/acceuil.jsp";
+    private static final String VUE1            = "/index.jsp";
+    private static final String ATT_UTILISATEUR = "utilisateur";
+    private static final String ATT_FORMULAIRE  = "form";
+    private static final String ATT_BEAN = "utilisateur";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+        // Creation de l'objet formulaireConnexion
+        FormulaireConnexion formulaireConnexion = new FormulaireConnexion();
+
+        /*
+         * Creation du bean utilisateur avec les données retournées par la
+         * methode connecterUtilisateur de l'objet formulaireConnexion
+         */
+        Utilisateur utilisateur = formulaireConnexion.connecterUtilisateur( request );
+        
+        /* Récupération de la session depuis la requète */
+        HttpSession session = request.getSession();
+
+        // Si le formulaire ne retourne aucune erreur
+        if ( formulaireConnexion.getErreurs().isEmpty() ) {
+
+            session.setAttribute( ATT_SESSION_USER, utilisateur );
+            // Mise en paramêtre du Bean utilisateur dans la requète
+            request.setAttribute( ATT_UTILISATEUR, utilisateur );
+            this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        }
+
+        /*Si des erreurs sont presente on soumet à nouveau la page d'authentification avec les
+         * erreurs presente dans le formualireConnexion
+         */
+        else
+            session.setAttribute( ATT_SESSION_USER, null );
+            request.setAttribute( ATT_BEAN, utilisateur);
+            request.setAttribute( ATT_FORMULAIRE, formulaireConnexion );
+            this.getServletContext().getRequestDispatcher( VUE1 ).forward( request, response );
+    }
+}
---
+++ StockVente/src/java/servlets/CreationClientServlet.java
@@ -0,0 +1,77 @@
+package projetTest.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+
+
+public class CreationClientServlet extends HttpServlet {
+
+    private static final String VUE1           = "/afficherClient.jsp";
+    private static final String VUE2           = "/creationClient.jsp";
+    private static final String VUE3            = "/index.jsp";
+    private static final String ATT_FORMULAIRE = "form";
+    private static final String ATT_BEAN       = "client";
+
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+        /**
+         * Instanciation de la classe FormulaireCreationClient
+         * Puis creation d'un client avec la méthode creerClient()
+         */
+        FormulaireCreationClient formulaireCreationClient = new FormulaireCreationClient();
+        Client client = formulaireCreationClient.creerClient( request );
+        
+        
+        
+        // Si le formulaire ne retourne aucune erreur
+       if ( formulaireCreationClient.getErreurs().isEmpty()  ) {
+
+            // Message afficher pour confirmation
+            formulaireCreationClient.setResultat( "Le client est creer avec succès" );
+
+            /** 
+             * Mise en paramètre du Bean utilisateur dans la requète
+             * Ainsi que du formulaire pour afficher les messages
+             */
+            request.setAttribute( ATT_BEAN, client );
+            request.setAttribute( ATT_FORMULAIRE, formulaireCreationClient );
+
+            // Renvoie a la jsp afficherClient.jsp
+            this.getServletContext().getRequestDispatcher( VUE1 ).forward( request, response );
+        }
+
+        /*
+         * Si des erreurs sont presente on soumet à nouveau la page
+         * d'authentification avec les erreurs presente dans le
+         * formualireConnexion
+         */
+        else
+        {
+            // Message afficher en cas d'erreur
+            formulaireCreationClient.setResultat( "Erreur lors de la creation du client" );
+
+        // Mise en paramètre du Bean utilisateur dans la requète
+
+        request.setAttribute( ATT_BEAN, client );
+        request.setAttribute( ATT_FORMULAIRE, formulaireCreationClient );
+
+        // Renvoie a la jsp creationClient.jsp
+        this.getServletContext().getRequestDispatcher( VUE2 ).forward( request, response );
+        }
+
+    }
+
+}
